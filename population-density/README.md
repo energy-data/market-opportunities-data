33c268f77df625d9cb5a64f28c4846d9916246b0
Olaf Veerman <olaf.veerman@gmail.com>
2016-06-09 12:47:20 -0400

    First approach to processing Worldpop data

---
+++ population-density/README.md
@@ -0,0 +1,14 @@
+## Population data
+A script to process population estimates from Worldpop. The script down-samples the data to a 100km2 grid and vectorizes it.
+
+### Usage
+1. Download population data from the Worldpop site to the root of the folder.
+2. Run `$ bash process.sh` to download the data and generate vector tiles.
+
+### About the data
+**Source:** World pop  
+**Description:** population estimates per 1km2 grid
+**Date:** April 2015  
+**URL:** http://www.worldpop.org.uk/data/summary/?contselect=Africa&countselect=Whole+Continent&typeselect=Population+2010
+**Type:** Shapefile  
+**License:** CC attribution 4.0
---
+++ population-density/process.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# This script processes Worldpop data for Africa. It down-samples it
+# to a 100km2 grid and averages the population estimate (pop per 100km2)
+# It then vectorizes the data and generates vector tiles.
+#
+# Requires the $SRC_FILE to be present in the root
+
+TMP_DIR="tmp"
+EXP_DIR="data"
+SRC_FILE="africa2010ppp.tif"
+DEST_FILE="data.mbtiles"
+
+
+error()
+{
+  echo >&2 $*
+  exit 1
+}
+
+# for portability and just in case which is not available
+typeset -r cmd_which="/usr/bin/which"
+[[ -x $cmd_which ]] || error "$cmd_which command not found"
+
+# check that every command is available and executable
+for command in ogr2ogr tippecanoe gdalwarp
+do
+  typeset -r cmd_$command=$($cmd_which $command)
+  [[ -x $(eval echo \$cmd_$command) ]] || error "$cmd_$command command not found"
+done
+
+
+# Check if there is a file with Worldpop data
+[[ -f $SRC_FILE ]] || error "You don't have the $SRC_FILE file with Worldpop data. Download it and place it in the root of this project."
+
+# The folder should not contain the tmp dir
+[[ ! -d $TMP_DIR ]] || error "It seems you already have a tmp directory. Remove it and run this script again."
+mkdir $TMP_DIR
+
+[[ ! -f $EXP_DIR/$DEST_FILE ]] || error "You already have a $EXP_DIR/$DEST_FILE file. Remove it and run this script again."
+mkdir -p $EXP_DIR
+
+
+# Re-sample the TIF to a 100km2 grid.
+# Original TIF in WGS 84, re-projection is necessary to make it equidistant (courtesy Matt Hanson)
+$cmd_gdalwarp africa2010ppp.tif $TMP_DIR/resampled-avg.tif -r average -t_srs "+proj=aea +lat_1=20 +lat_2=-23 +lat_0=0 +lon_0=25 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs" -tr 10000 10000 
+
+# Polygonizing will turn floating point to an integer
+# Multiply by 100 to retain some precision
+gdal_calc.py -A $TMP_DIR/resampled-avg.tif --outfile=$TMP_DIR/resampled-total.tif --calc="A*100" --NoDataValue=0
+
+# Create a vector from the TIF with total population per 100km2
+gdal_polygonize.py $TMP_DIR/resampled-total.tif -f "GeoJSON" $TMP_DIR/resampled.geojson fieldname "pop100km2"
+
+# Ensure that the correct projection is set
+$cmd_ogr2ogr -f "GeoJSON" $EXP_DIR/data.geojson $TMP_DIR/resampled.geojson -s_srs "+proj=aea +lat_1=20 +lat_2=-23 +lat_0=0 +lon_0=25 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs" -t_srs EPSG:4326
+
+# Tippecanoe the thing
+echo "Convert GeoJSON to mbtiles"
+$cmd_tippecanoe -o $EXP_DIR/data.mbtiles $EXP_DIR/data.geojson -P
+
+rm -r $TMP_DIR
