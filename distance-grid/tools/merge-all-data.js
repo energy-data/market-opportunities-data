e3480aaec95d786926aae8f7e0c863c598aeeb3f
Olaf Veerman <olaf.veerman@gmail.com>
2016-10-11 15:38:22 -0400

    Improve merge tool

--- distance-grid/tools/merge-all-data.js
+++ distance-grid/tools/merge-all-data.js
@@ -1,11 +1,12 @@
 // quick script to merge all geojson files in the data folder
 'use strict'
+const filterRings = require('../../tools/filter-inner-rings.js')
 const fs = require('fs')
 const turf = require('turf')
 
 var finalData = []
 for (let fn of fs.readdirSync('../data')) {
   let cData = JSON.parse(fs.readFileSync('../data/' + fn))
-  cData.features.map(f => finalData.push(f))
+  cData.features.map(f => finalData.push(filterRings(f, 1000000)))
 }
-fs.writeFileSync('../osm-power-merged.json', JSON.stringify(turf.featureCollection(finalData)))
+fs.writeFileSync('../osm-power-merged-filtered.json', JSON.stringify(turf.featureCollection(finalData)))
